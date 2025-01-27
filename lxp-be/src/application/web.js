935ec172b6efa8eb8ac46ca5d8b298571b5b335b
Arya <ngurahpratama2002@gmail.com>
2025-01-27 20:40:32 +0700

    Done register

--- lxp-be/src/application/web.js
+++ lxp-be/src/application/web.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { errorMiddleware } from "../middleware/error-middleware";
-import { publicRouter } from "../route/public-api";
+import { errorMiddleware } from "../middleware/error-middleware.js";
+import { publicRouter } from "../route/public-api.js";
 
 const web = express();
 web.use(express.json());
