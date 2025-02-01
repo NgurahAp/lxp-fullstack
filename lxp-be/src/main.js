9be07f412350b9f76e6125aa129933de042a99c2
Arya <ngurahpratama2002@gmail.com>
2025-02-01 18:25:17 +0700

    Done adding nextjs into our app

--- lxp-be/src/main.js
+++ lxp-be/src/main.js
@@ -1,6 +1,6 @@
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-web.listen(3000, () => {
-  logger.info("App start in port 3000");
+web.listen(3001, () => {
+  logger.info("App start in port 3001");
 });
