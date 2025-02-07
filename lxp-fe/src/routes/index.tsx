859b41705a9f6b2a46dbb9f7f25d4bf455ed795b
Arya <ngurahpratama2002@gmail.com>
2025-02-07 11:09:30 +0700

    Done Protected Route

--- lxp-fe/src/routes/index.tsx
+++ lxp-fe/src/routes/index.tsx
@@ -1,8 +1,10 @@
+// AppRoutes.tsx
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import { Login } from "../pages/auth/Login";
 import Dashboard from "../pages/dashboard";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -11,7 +13,14 @@ const AppRoutes: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </MainLayout>
     </Router>
